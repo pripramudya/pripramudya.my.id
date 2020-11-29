20dd64e7bf6ed866e2af09afa8ec6512ae189742
pripramudya <pripramudya@gmail.com>
2020-11-29 13:57:11 +0700

    hotfix ebook-web

---
+++ static/mahir-analisis-data/index.js
@@ -0,0 +1,28 @@
+gitbook.require(["gitbook"], function(gitbook) {
+gitbook.start({
+"sharing": {
+"github": false,
+"facebook": true,
+"twitter": true,
+"google": false,
+"linkedin": false,
+"weibo": false,
+"instapper": false,
+"vk": false,
+"all": ["facebook", "google", "twitter", "linkedin", "weibo", "instapaper"]
+},
+"fontsettings": {
+"theme": "white",
+"family": "sans",
+"size": 2
+},
+"edit": {
+"link": null,
+"text": null
+},
+"download": null,
+"toc": {
+"collapse": "subsection"
+}
+});
+});
